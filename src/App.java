6deccb24cb2d0af14b5946b08c8ef54e6f140d36
IsabelUllauri1 <iullaurib@est.ups.edu.ec>
2025-06-19 09:13:22 -0500

    final commit

--- src/App.java
+++ src/App.java
@@ -2,6 +2,9 @@ import materia.controllers.ArbolBinario;
 
 public class App {
     public static void main(String[] args) throws Exception {
+
+        System.out.println("Nombre: Isabel Ullauri");
+        
         ArbolBinario aB = new ArbolBinario();
         aB.insert(50);
         aB.insert(17);
@@ -11,11 +14,20 @@ public class App {
         aB.insert(54);
         aB.insert(14);
         aB.insert(19);
-
+        System.out.println("La altura es= "+aB.getHeight());
+        System.out.println("El peso es: "+ aB.getPeso());
         aB.imprimirArbol();
         
         System.out.println(aB.buscar(1));
         System.out.println(aB.buscar(19));
+        aB.imprimirInOrderConAltura();
+        aB.imprimirFactorDeEquilibrio();
 
+        System.out.println("El arbol esta quilibrado: "+ aB.arbolBalanceado());
+        System.out.println("agregamos valor: 15");
+        aB.insert(15);
+        aB.imprimirFactorDeEquilibrio();
+        //System.out.println("nodos desequilibrados: " + aB.nodosDes();
+        aB.nodosDes();
     }
 }
--- src/materia/controllers/ArbolBinario.java
+++ src/materia/controllers/ArbolBinario.java
@@ -1,5 +1,8 @@
 package materia.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import materia.models.Node;
 // va preguntando si va a la derecha o izquierda. pE: LE DICEN DERECHA, entonces pregunta si es nullo. si no 
 //es nullo, pregunta si tiene hijos, y de nuevo derecha o izquierda
@@ -28,6 +31,7 @@ public class ArbolBinario {
 
     private Node insertRec(Node padre, int value){
         if(padre==null){
+            peso++;
             return new Node(value);
         }
         
@@ -50,16 +54,16 @@ public class ArbolBinario {
 
     public void imprimirArbol(){
         imprimirRec(root);
+        System.out.println();
 
     }
 
     private void imprimirRec(Node nodo){
 
         if(nodo!=null){
-            System.out.println(nodo.getValue()+",");
-            imprimirRec(nodo.getLeft());            
+            imprimirRec(nodo.getLeft()); 
+            System.out.print(nodo.getValue()+",");
             imprimirRec(nodo.getRight());
-            
         }
 
     }
@@ -97,9 +101,85 @@ public class ArbolBinario {
         int rightHeight= getHeightRec(node.getRight());
         return Math.max(leftHeight, rightHeight)+1;            
     }
-    public void imprimirPesoYValor(){
+
+    public void imprimirInOrderConAltura(){
+
+        imprimirInOrderConAlturaRec(root);
+
+    }    
+
+
+    private void imprimirInOrderConAlturaRec(Node nodo){
+        if (nodo!= null){
+            imprimirInOrderConAlturaRec(nodo.getLeft());
+            int height= getHeightRec(nodo);
+            System.out.print(nodo.getValue() + " (h= "+ height+ ")");
+            imprimirInOrderConAlturaRec(nodo.getRight());
+
+        }
         
     }
+    private int getBalance(Node nodo){
+        if(nodo==null)return 0;
+        return getHeightRec(nodo.getLeft())-getHeightRec(nodo.getRight());
+    }
+
+    public void imprimirFactorDeEquilibrio(){
+        imprimirFactorDeEquilibrioRec(root);
+    }
+
+    private void imprimirFactorDeEquilibrioRec(Node nodo){
+        if(nodo != null){
+            imprimirFactorDeEquilibrioRec(nodo.getLeft());
+            int balance= getBalance(nodo);
+            System.out.println("nodo: " + nodo.getValue()+ " |Balance: "+ balance);
+            imprimirFactorDeEquilibrioRec(nodo.getRight());
+
+        }
+
+    }
+    
+    public boolean arbolBalanceado(){
+        return arbolBalanceadoRec(root);
+    }
+
+    private boolean arbolBalanceadoRec(Node nodo){
+        if(nodo == null )return true;
+        int balanceado= Math.abs(getBalance(nodo));
+        if(balanceado>1 )return false;
+
+        return arbolBalanceadoRec(nodo.getLeft()) && arbolBalanceadoRec(nodo.getRight());
+
+    }
+    public void nodosDes (){
+        List<Node> bad =new ArrayList<>();
+        nodosDesRec(root, bad);
+
+        if(bad.isEmpty()){
+            System.out.println(" no hay nada");
+
+        }else{
+            System.out.print("noods desbalanceados: ");
+            for(int i=0; i< bad.size(); i++){
+                Node n = bad.get(i);
+                int bf = getBalance(n);
+                System.out.print(n.getValue()+bf);
+                if(i< bad.size()-1); System.out.print(" y ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    private void nodosDesRec(Node nodo, List<Node> dest){
+        if(nodo != null){
+            if(Math.abs(getBalance(nodo))>1) dest.add(nodo);
+            nodosDesRec(nodo.getLeft(), dest);
+            nodosDesRec(nodo.getRight(), dest);
+        } 
+    }
+
+    
 
     
 
